55447238bcaacc665fba020d42f7a799820de904
Tavishi Vemuri <tavishivss981@gmail.com>
2022-02-24 22:01:21 +0530

    arrays in js

--- try.js
+++ try.js
@@ -1,26 +1,14 @@
+var fruits=["apple","banana","papaya"]
+function loadfruits()
+{
+    document.getElementById('fruits').innerHTML=fruits;
 
+}
 
-function upDate(previewPic){
-    /* 
-       1) change the url for the background image of the div with the id = "image" 
-       to the source file of the preview image
-       
-       2) Change the text  of the div with the id = "image" 
-       to the alt text of the preview image 
-       */
-       document.getElementById('image').style.backgroundImage="url(" + previewPic.src + ")";
-      document.getElementById('image').innerHTML = previewPic.alt;
-     }
-   
-     function unDo(){
-        /* 
-       1) Update the url for the background image of the div with the id = "image" 
-       back to the orginal-image. 
-       
-       2) Change the text  of the div with the id = "image" 
-       back to the original text. 
-       */
-      document.getElementById('image').style.backgroundImage="url('')"
-      document.getElementById('image').innerHTML ="Hover over an image below to display here."
-       
-     }
+function addfruits()
+{
+  var fruit=prompt("enter fruit name: ");
+  fruits.push(fruit);
+  document.getElementById('fruits').innerHTML=fruits;
+  
+}
