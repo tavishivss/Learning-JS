585d270375bc9e84ae577015e6a55102af0ae0c9
Tavishi Vemuri <tavishivss981@gmail.com>
2022-02-24 20:05:17 +0530

    gallery

--- try.js
+++ try.js
@@ -1,4 +1,26 @@
-function message(msg)
-{
-    document.getElementById('click').innerHTML = "Clicked "+msg+" button";
-}
+
+
+function upDate(previewPic){
+    /* 
+       1) change the url for the background image of the div with the id = "image" 
+       to the source file of the preview image
+       
+       2) Change the text  of the div with the id = "image" 
+       to the alt text of the preview image 
+       */
+       document.getElementById('image').style.backgroundImage="url(" + previewPic.src + ")";
+      document.getElementById('image').innerHTML = previewPic.alt;
+     }
+   
+     function unDo(){
+        /* 
+       1) Update the url for the background image of the div with the id = "image" 
+       back to the orginal-image. 
+       
+       2) Change the text  of the div with the id = "image" 
+       back to the original text. 
+       */
+      document.getElementById('image').style.backgroundImage="url('')"
+      document.getElementById('image').innerHTML ="Hover over an image below to display here."
+       
+     }
