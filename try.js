e6944539e88b9d7bde4dd9a360c9b9128ec30417
Tavishi Vemuri <tavishivss981@gmail.com>
2022-02-26 21:48:38 +0530

    forms in html

--- try.js
+++ try.js
@@ -1,25 +1,25 @@
-var num=[10,20,60]
+// var num=[10,20,60]
 
-function numb()
-{
-  document.getElementById('add').innerHTML=num;
-}
+// function numb()
+// {
+//   document.getElementById('add').innerHTML=num;
+// }
 
-function avg()
-{
-  var sum=0,count=0;
-  if (num.length > 0){
-    for (index = 0; index < num.length; index++){
-      if (num[index] != undefined){
-        sum += num[index]; 
-        count = count + 1;
-      }
-    }
-    document.write(sum/count);
-  }
-  else
-    document.write("Empty Array");
+// function avg()
+// {
+//   var sum=0,count=0;
+//   if (num.length > 0){
+//     for (index = 0; index < num.length; index++){
+//       if (num[index] != undefined){
+//         sum += num[index]; 
+//         count = count + 1;
+//       }
+//     }
+//     document.write(sum/count);
+//   }
+//   else
+//     document.write("Empty Array");
   
 
-}
+// }
 
