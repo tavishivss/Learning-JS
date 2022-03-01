2348d270563020e1a4aabb2858b8d5c0e488c603
Tavishi Vemuri <tavishivss981@gmail.com>
2022-03-01 18:40:12 +0530

    verification

--- try.js
+++ try.js
@@ -1,25 +1,13 @@
-// var num=[10,20,60]
-
-// function numb()
-// {
-//   document.getElementById('add').innerHTML=num;
-// }
-
-// function avg()
-// {
-//   var sum=0,count=0;
-//   if (num.length > 0){
-//     for (index = 0; index < num.length; index++){
-//       if (num[index] != undefined){
-//         sum += num[index]; 
-//         count = count + 1;
-//       }
-//     }
-//     document.write(sum/count);
-//   }
-//   else
-//     document.write("Empty Array");
-  
-
-// }
-
+function nic(){
+  if(nickname.checked)
+  {
+      nn.style.display="inline";
+      nyc.setAttribute('required',true);
+  }
+  else
+  {
+      nyc.removeAttribute('required');
+      nn.style.display="none";
+      
+  }
+}
