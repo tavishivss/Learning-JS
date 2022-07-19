66a454f22e59f3defe3bc90956aac4da0c19f743
Tavishi Vemuri <tavishivss981@gmail.com>
2022-07-20 01:03:08 +0530

    scoreboard

---
+++ BB-scoreboard/script.js
@@ -0,0 +1,41 @@
+let count=0;
+function reset()
+{
+    count=0;
+    let a=document.getElementById('num').innerHTML=count;
+}
+function reset1()
+{
+    count=0;
+    let a=document.getElementById('num1').innerHTML=count;
+}
+function first()
+{
+   count+=1;
+   let a=document.getElementById('num').innerHTML=count;
+}
+function second()
+{  
+   count+=2;
+   let a=document.getElementById('num').innerHTML=count;
+}
+function third()
+{  
+   count+=3;
+   let a=document.getElementById('num').innerHTML=count;
+}
+function first1()
+{
+   count+=1;
+   let a=document.getElementById('num1').innerHTML=count;
+}
+function second2()
+{  
+   count+=2;
+   let a=document.getElementById('num1').innerHTML=count;
+}
+function third3()
+{  
+   count+=3;
+   let a=document.getElementById('num1').innerHTML=count;
+}
